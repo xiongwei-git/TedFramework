e79d7af0e690b8431c8f3ffbaabf8dfbdf83c887
Ted <xiong-wei@hotmail.com>
2014-09-03 19:57:40 +0800

    com
    com

---
+++ src/com/android/TedFramework/util/StringUtil.java
@@ -0,0 +1,8 @@
+package com.android.TedFramework.util;
+
+public class StringUtil {
+
+    public static boolean emptyOrNull(String str) {
+        return str == null || str.length() == 0;
+    }
+}
