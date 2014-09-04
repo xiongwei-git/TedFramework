ed062133833761f5cd5c5fb4e1dff6b47137b62a
xiongwei-git <xiong-wei@hotmail.com>
2014-09-05 02:39:04 +0800

    home
    home

---
+++ src/com/android/TedFramework/util/CheckDoubleClick.java
@@ -0,0 +1,17 @@
+package com.android.TedFramework.util;
+
+/**
+ * Created by w_xiong on 2014/9/5.
+ */
+public class CheckDoubleClick {
+    private static long lastClickTime=0;
+    public static boolean isFastDoubleClick() {
+        long time = System.currentTimeMillis();
+        long timeD = time - lastClickTime;
+        if ( 0 < timeD && timeD < 500) {
+            return true;
+        }
+        lastClickTime = time;
+        return false;
+    }
+}
